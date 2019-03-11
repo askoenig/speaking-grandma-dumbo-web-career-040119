5fb6f126574784a545ae813cb762a94d46b9a331
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-11 14:12:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(words)
-  if words == words.upcase
+  if words == "I LOVE YOU GRANDMA!"
+  return"I LOVE YOU TOO PUMPKIN!" 
+  elsif words == words.upcase
   return "NO, NOT SINCE 1938!"
-  elsif words == "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN!"
 else 
   return "HUH!? SPEAK UP, SONNY!"
 end
